8c0fad203b75622fb4c135ae9194e5c7d34e2ba3
matijatomc <55134628+matijatomc@users.noreply.github.com>
2020-02-22 22:07:01 +0100

    Add files via upload

---
+++ tomcMatijaNiz4/tomcMatijaNiz4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tomcMatijaNiz4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] niz = new int[11, 11];
+            for (int i = 0; i < 11; i++)
+            {
+                for (int j = 0; j < 11; j++)
+                {
+                    niz[i, j] = i * j;
+                }
+            }
+            Console.WriteLine("Vrijednost upisana na poziciji 7*5 u tablici 11*11 je: " + niz[7, 5]);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ tomcMatijaNiz5/tomcMatijaNiz5/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tomcMatijaNiz5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] niz = new int[10];
+            int[] niz2 = new int[10];
+            int neparni = 0;
+            int parni = 0;
+            int br;
+            bool istina = true;
+
+            Console.WriteLine("Upisite 20 brojeva ili uspisite brojeve sve dok ne upisete 0:");
+            while (neparni != 10 && parni != 10 && istina)
+            {
+                br = Convert.ToInt32(Console.ReadLine());
+                if (br == 0)
+                {
+                    istina = false;
+                }
+                if (br % 2 != 0)
+                {
+                    niz[neparni] = br;
+                    neparni++;
+                }
+                if (br % 2 == 0)
+                {
+                    niz2[parni] = br;
+                   parni++;
+                }
+            }
+            Console.WriteLine("Svi neparni brojevi koje ste upisali:");
+            foreach (int g in niz) Console.Write(g + " ");
+            Console.WriteLine("");
+            Console.WriteLine("Svi psrni brojevi koje ste upisali:");
+            foreach (int h in niz2) Console.Write(h + " ");
+            Console.ReadKey();
+        }
+    }
+}
